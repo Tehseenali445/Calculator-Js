6b4f2040c147e1a2035398c1cd3336a7b09d4faa
Tehseen Ali <154703743+Tehseenali445@users.noreply.github.com>
2024-03-15 23:57:07 +0500

    Calculator
    Calculator-Js

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+let num01 =  +prompt("Enter Your First Number");
+let num02 = +prompt("Enter Your Second Number");
+
+let sum = num01 + num02;
+document.write("Sum Of " + sum + " Numbers " + "<br/>" + "<br/>"); 
+
+let sub = num01 - num02;
+document.write("Subtraction of Numbers " +  sub + "<br/>" + "<br/>"); 
+
+let div = num01 / num02;
+document.write("Divide of Numbers " + div + "<br/>" + "<br/>"); 
+
+let Reminder = num01 % num02;
+document.write("Reminder of Numbers " + Reminder + "<br/>" + "<br/>"); 
+
+console.log(document.write)
